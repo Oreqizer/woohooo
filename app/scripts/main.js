874edc1d3f9ff66e3a56f5895b42e3c6af627dd8
Oreqizer <oreqizer@gmail.com>
2015-07-12 19:01:04 +0200

    updated height

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,38 @@
 /* jshint devel:true */
+"use strict";
 
+$(function() {
+  
+  // Variables:
+  var height,
+      width,
+      win = $(window),
+      doc = $(document),
+      
+      woohooo = $('#woohooo');
+  
+  // Main events:
+  win.on("resize", function() {
+    windowDimensions();
+  });
+  
+  // Parallax:
+  win.on("scroll", function() {
+    if (doc.scrollTop() >= height/2) {
+      woohooo.hide();
+    } else {
+      woohooo.show();
+    }
+  });
+  
+  // Initialisation:
+  windowDimensions();
+  
+  // Helper functions:
+  function windowDimensions() {
+    height = win.height();
+    width = win.width();
+    console.log (height + ', ' + width);
+  }
+  
+});
